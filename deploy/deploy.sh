707079e6a4bb8182bc06cc8cecc6c2bc110cb53d
Anton Nemtsev <thesilentimp@gmail.com>
2017-04-07 18:13:36 +0300

    fixed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,19 +10,16 @@ mkdir ${RESULT_DIR}
 shopt -s extglob
 mv -f !(${RESULT_DIR}) ./${RESULT_DIR}
 tar -czf ${ARCH_NAME} ${RESULT_DIR}
-rm ./${ARCH_NAME};
 sshpass -e scp -C -o StrictHostKeyChecking=no ${ARCH_NAME} ${SSH_USER}@${SSH_IP}:${WEB_PATH}
 sshpass -e ssh -tt -C ${SSH_USER}@${SSH_IP} << EOF
 cd /;
-command pwd >&1
 cd ${WEB_PATH};
-command pwd >&1
 tar -xzf ./${ARCH_NAME} -C ./;
+rm ./${ARCH_NAME};
 if [ ! -f ".env" ]; then
     echo WEB_PATH=${WEB_PATH} >> .env;
 fi
 cd ${RESULT_DIR};
-command pwd >&1
 nvm install 7
 nvm use 7
 nvm alias default 7
@@ -30,11 +27,9 @@ npm install;
 npm run build;
 npm run server;
 cd ..
-command pwd >&1
 rm -dRf ${SYMLINK_NAME}
 ln -ds ${RESULT_DIR} ./${SYMLINK_NAME}
 cd ./${SYMLINK_NAME}
-command pwd >&1
 pm2 stop ${PROCESS_NAME}
 pm2 delete ${PROCESS_NAME}
 pm2 start ./server/server.js --name="${PROCESS_NAME}" --watch
