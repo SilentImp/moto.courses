273db2b387a9bc64dcec39a5187ad30cfa220c78
Anton Nemtsev <thesilentimp@gmail.com>
2017-04-10 23:04:00 +0300

    service worker path check

--- source/components/layout/javascript-inline/worker_starter.js
+++ source/components/layout/javascript-inline/worker_starter.js
@@ -1,6 +1,6 @@
 (function () {
     if ( typeof SharedWorker !== "function" ) return;
-    navigator.serviceWorker.register( "/javascript/work.js" , { scope: '/' } ).then( function( reg ) {
+    navigator.serviceWorker.register( "/javascript/worker.js" , { scope: '/' } ).then( function( reg ) {
         console.log( 'Регистрация сработала ' + reg.scope );
     }).catch( function( error ) {
         console.log( 'Регистрация прошла неудачно ' + error );
