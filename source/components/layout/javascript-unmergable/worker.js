1cd631de9b8476ed506d90be9a63f479ebc97080
Anton Nemtsev <thesilentimp@gmail.com>
2017-04-10 23:50:50 +0300

    scope

--- source/components/layout/javascript-unmergable/worker.js
+++ source/components/layout/javascript-unmergable/worker.js
@@ -1,7 +1,7 @@
 self.addEventListener( 'activate', function (event) {
     console.log('Activate');
+});
 
-    self.addEventListener( 'fetch', function (event) {
-        console.log('this Handling fetch event for: ', event.request.url);
-    });
+self.addEventListener( 'fetch', function (event) {
+    console.log('this Handling fetch event for: ', event.request.url);
 });
