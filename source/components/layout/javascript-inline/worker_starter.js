b7c01acc756d113781c213ee092f741759924bba
Anton Nemtsev <thesilentimp@gmail.com>
2017-04-12 20:24:58 +0300

    FF cache fix

--- source/components/layout/javascript-inline/worker_starter.js
+++ source/components/layout/javascript-inline/worker_starter.js
@@ -1,6 +1,6 @@
 (function () {
     if ( typeof SharedWorker !== "function" ) return;
-    navigator.serviceWorker.register( "/worker.js" , { scope: '/' } ).then( function( reg ) {
+    serviceWorker.register( "/worker.js" , { scope: '/' } ).then( function( reg ) {
         console.log( 'Регистрация сработала для скоупа: ' + reg.scope );
     }).catch( function( error ) {
         console.log( 'Регистрация прошла неудачно ' + error );
