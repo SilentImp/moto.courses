3eb3ff86d4ed87ded3b68da0fac19d87170c1e2b
Anton Nemtsev <thesilentimp@gmail.com>
2017-04-13 14:21:39 +0300

    head allowed

--- server_source/server.js
+++ server_source/server.js
@@ -15,7 +15,7 @@ app.listen(PORT, () => {
 // Set crossorigin headers
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
