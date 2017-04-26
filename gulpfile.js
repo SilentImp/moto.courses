4c5a80d7e4c8340427df8ef59c0a4f6c76177b18
Anton Nemtsev <silentimp@MacBook-Pro-Anton.local>
2017-04-26 17:32:36 +0300

    error inside

--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,174 @@
 const gulp = require('gulp')
-    , babel = require('gulp-babel')
-    , pug = require('gulp-pug')
-    , sprite = require('gulp-svgsprite')
-    , cheerio = require('gulp-cheerio')
-    , svgmin = require('gulp-svgmin')
-    , replace = require('gulp-replace')
-    , htmlclean = require('gulp-htmlclean')
-    , rename = require('gulp-rename')
-    , concat = require('gulp-concat')
-    , sourcemaps = require('gulp-sourcemaps')
-    , postcss = require('gulp-postcss')
-    , order = require("gulp-order")
-    , imagemin = require('gulp-imagemin')
-    , uglify = require('gulp-uglify')
-    , imageResize = require('gulp-image-resize')
-    , resizeImageTasks = []
-    , paths = {
-        source: {
-            images:             ["./source/components/**/*.jpg", "./source/components/**/*.png"],
-            components_css:    "./source/components/**/*.css",
-            all_css:            ["./source/components/**/*.css", "./source/helpers/**/*.css"],
-            all_js:             ["./source/components/**/*.js"],
-            js:                 "./source/components/**/javascript/**/*.js",
-            jss:                "./source/components/**/javascript-unmergable/**/*.js",
-            html:               "./source/pages/**/*.pug",
-            all_html:           ["./source/pages/**/*.pug", "./source/components/**/*.pug"],
-            sprite:             "./source/svg-sprites/**/*.svg",
-            fonts:              ["./source/fonts/**/*.woff", "./source/fonts/**/*.woff2"],
-            server:             "./server_source/*.js",
-            favicon:            "./source/favicon/**/*",
-            static:             "./source/static/**/*"
-        },
-        build: {
-            build:              "./build/",
-            sprite:             "./source/tmp/",
-            css:                "./build/styles/",
-            images:             "./build/images/",
-            fonts:              "./build/fonts/",
-            js:                 "./build/javascript/",
-            favicon:            "./build/",
-            server:             "./server/",
-            static:             "./build/"
-        }
-    };
+  , babel = require('gulp-babel')
+  , pug = require('gulp-pug')
+  , sprite = require('gulp-svgsprite')
+  , cheerio = require('gulp-cheerio')
+  , svgmin = require('gulp-svgmin')
+  , replace = require('gulp-replace')
+  , htmlclean = require('gulp-htmlclean')
+  , jslint = require('gulp-jslint')
+  , jshint = require('gulp-jshint')
+  , eslint = require('gulp-eslint')
+  , rename = require('gulp-rename')
+  , concat = require('gulp-concat')
+  , sourcemaps = require('gulp-sourcemaps')
+  , postcss = require('gulp-postcss')
+  , order = require("gulp-order")
+  , imagemin = require('gulp-imagemin')
+  , uglify = require('gulp-uglify')
+  , imageResize = require('gulp-image-resize')
+  , resizeImageTasks = []
+  , paths = {
+    source: {
+      images:             ["./source/components/**/*.jpg", "./source/components/**/*.png"],
+      components_css:    "./source/components/**/*.css",
+      all_css:            ["./source/components/**/*.css", "./source/helpers/**/*.css"],
+      all_js:             ["./source/components/**/*.js"],
+      js:                 "./source/components/**/javascript/**/*.js",
+      jss:                "./source/components/**/javascript-unmergable/**/*.js",
+      html:               "./source/pages/**/*.pug",
+      all_html:           ["./source/pages/**/*.pug", "./source/components/**/*.pug"],
+      sprite:             "./source/svg-sprites/**/*.svg",
+      fonts:              ["./source/fonts/**/*.woff", "./source/fonts/**/*.woff2"],
+      server:             "./server_source/*.js",
+      favicon:            "./source/favicon/**/*",
+      static:             "./source/static/**/*"
+    },
+    build: {
+      build:              "./build/",
+      sprite:             "./source/tmp/",
+      css:                "./build/styles/",
+      images:             "./build/images/",
+      fonts:              "./build/fonts/",
+      js:                 "./build/javascript/",
+      favicon:            "./build/",
+      server:             "./server/",
+      static:             "./build/"
+    }
+  };
 
 gulp.task('sprite', function() {
-	return gulp.src( paths.source.sprite )
-        .pipe( svgmin({
-			js2svg: {
-				pretty: true
-			}
-		}))
-        .pipe( cheerio({
-			run: function ($) {
-				$('[fill]').removeAttr('fill');
-				$('[style]').removeAttr('style');
-			},
-			parserOptions: { xmlMode: true }
-		}))
-        .pipe( replace('&gt;', '>'))
-        .pipe( sprite({
-            inlineSvg: true
-            , preview: false
-            , mode: "symbols"
-            , metaAttrs: ["width", "height", "fill"]
-            , cleanAttrs: ["fill", "style"]
-        }))
-        .pipe( gulp.dest(paths.build.sprite) );
+  return gulp.src( paths.source.sprite )
+    .pipe( svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    .pipe( cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe( replace('&gt;', '>'))
+    .pipe( sprite({
+      inlineSvg: true
+      , preview: false
+      , mode: "symbols"
+      , metaAttrs: ["width", "height", "fill"]
+      , cleanAttrs: ["fill", "style"]
+    }))
+    .pipe( gulp.dest(paths.build.sprite) );
 });
 
 gulp.task('html', ['sprite'], function() {
-	return gulp.src( paths.source.html )
-        .pipe( pug({}) )
-        .pipe( htmlclean({}) )
-        .pipe( gulp.dest(paths.build.build) );
+  return gulp.src( paths.source.html )
+    .pipe( pug({}) )
+    .pipe( htmlclean({}) )
+    .pipe( gulp.dest(paths.build.build) );
 });
 
 gulp.task('css', function() {
-    let plugins = [
-        require('precss')
-        , require('autoprefixer')
-        , require('postcss-discard-empty')
-        , require('postcss-clean')
-    ];
+  let plugins = [
+    require('precss')
+    , require('autoprefixer')
+    , require('postcss-discard-empty')
+    , require('postcss-clean')
+  ];
 
-	return gulp.src( paths.source.components_css )
-        .pipe( postcss(plugins) )
-        .pipe( order(['reset.css']) )
-        .pipe( concat('styles.css') )
-        .pipe( gulp.dest(paths.build.css) );
+  return gulp.src( paths.source.components_css )
+    .pipe( postcss(plugins) )
+    .pipe( order(['reset.css']) )
+    .pipe( concat('styles.css') )
+    .pipe( gulp.dest(paths.build.css) );
 });
 
-gulp.task('javascript', function() {
-	return gulp.src( paths.source.js )
-        .pipe( babel({
-            presets: ['latest']
-        }))
-        .pipe( concat('scripts.js') )
-        .pipe( uglify() )
-        .pipe( gulp.dest(paths.build.js) );
+gulp.task('javascript', ['lint_js'], function() {
+  return gulp.src( paths.source.js )
+    .pipe( babel({
+      presets: ['latest']
+    }))
+    .pipe( concat('scripts.js') )
+    .pipe( uglify() )
+    .pipe( gulp.dest(paths.build.js) );
+});
+
+gulp.task('lint_js', function() {
+  return gulp.src( paths.source.js )
+  
+    .pipe(jshint({
+      esversion: 6,
+      laxcomma: true,
+      browser : true,
+      indent : 4,
+      maxerr: 0,
+      maxlen: 120,
+      passfail: true
+    }))
+    .pipe(jshint.reporter('default'))
+
+    .pipe(eslint({
+      envs: ["browser", "es6"],
+      rules: {
+          "one-var": 0,
+          "no-new": 0,
+          "semi": ["error", "always"],
+          "comma-style": ["error", "first"],
+          "padded-blocks": ["error", "never"]
+      }
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
 });
 
 gulp.task('javascript-standalone', function() {
-	return gulp.src( paths.source.jss )
-        .pipe( babel({
-            presets: ['latest']
-        }))
-        .pipe( uglify() )
-        .pipe( rename({dirname: ''}) )
-        .pipe( gulp.dest(paths.build.build) );
+  return gulp.src( paths.source.jss )
+    .pipe( babel({
+      presets: ['latest']
+    }))
+    .pipe( uglify() )
+    .pipe( rename({dirname: ''}) )
+    .pipe( gulp.dest(paths.build.build) );
 });
 
 gulp.task('fonts', function() {
-	return gulp.src( paths.source.fonts )
-        .pipe( gulp.dest(paths.build.fonts) );
+  return gulp.src( paths.source.fonts )
+    .pipe( gulp.dest(paths.build.fonts) );
 });
 
 gulp.task('images', function() {
-	return gulp.src( paths.source.images, { base: './' } )
-        .pipe( imagemin({
-            interlaced: true,
-            progressive: true,
-            optimizationLevel: 5
-        }))
-        .pipe( rename({dirname: ''}) )
-        .pipe( gulp.dest('./build/images/') );
+  return gulp.src( paths.source.images, { base: './' } )
+    .pipe( imagemin({
+      interlaced: true,
+      progressive: true,
+      optimizationLevel: 5
+    }))
+    .pipe( rename({dirname: ''}) )
+    .pipe( gulp.dest('./build/images/') );
 });
 
 [1270, 676].forEach(function(size) {
   const resizeImageTask = 'resize_' + size;
   gulp.task(resizeImageTask, function() {
-    return gulp.src(paths.source.images)
-        .pipe(imageResize({
-            width:  size,
-            imageMagick: false,
-            upscale: false
-        }))
-        .pipe( rename({dirname: "", suffix: (size == 1270) ? '@2x' : ''}) )
-        .pipe( gulp.dest('./build/images/') );
+  return gulp.src(paths.source.images)
+    .pipe(imageResize({
+      width:  size,
+      imageMagick: false,
+      upscale: false
+    }))
+    .pipe( rename({dirname: "", suffix: (size == 1270) ? '@2x' : ''}) )
+    .pipe( gulp.dest('./build/images/') );
   });
   resizeImageTasks.push(resizeImageTask);
 });
@@ -145,33 +176,33 @@ gulp.task('images', function() {
 gulp.task('images', resizeImageTasks);
 
 gulp.task('favicon', function() {
-	return gulp.src( paths.source.favicon )
-        .pipe( gulp.dest(paths.build.favicon) );
+  return gulp.src( paths.source.favicon )
+    .pipe( gulp.dest(paths.build.favicon) );
 });
 
 gulp.task('static', function() {
-	return gulp.src( paths.source.static )
-        .pipe( rename({dirname: ""}) )
-        .pipe( gulp.dest(paths.build.static) );
+  return gulp.src( paths.source.static )
+    .pipe( rename({dirname: ""}) )
+    .pipe( gulp.dest(paths.build.static) );
 });
 
 gulp.task('default', ['javascript'], function() {
-    gulp.watch( paths.source.js, ['javascript'] );
+  gulp.watch( paths.source.js, ['javascript'] );
 });
 
 gulp.task('build', ['fonts', 'javascript', 'javascript-standalone', 'css', 'html', 'images', 'favicon', 'static']);
 
 gulp.task('watch', function() {
-    gulp.watch( paths.source.all_js, ['javascript','javascript-standalone'] );
-    gulp.watch( paths.source.all_css, ['css'] );
-    gulp.watch( paths.source.all_html, ['html'] );
+  gulp.watch( paths.source.all_js, ['javascript','javascript-standalone'] );
+  gulp.watch( paths.source.all_css, ['css'] );
+  gulp.watch( paths.source.all_html, ['html'] );
 });
 
 gulp.task('server', function() {
-	return gulp.src( paths.source.server )
-        .pipe( babel({
-            presets: ['latest']
-        }))
-        .pipe( uglify() )
-        .pipe( gulp.dest(paths.build.server) );
+  return gulp.src( paths.source.server )
+    .pipe( babel({
+      presets: ['latest']
+    }))
+    .pipe( uglify() )
+    .pipe( gulp.dest(paths.build.server) );
 });
--- source/components/abstract/javascript/abstract.js
+++ source/components/abstract/javascript/abstract.js
@@ -1,99 +1,95 @@
-"use strict";
 (function () {
+  'use strict';
+  /**
+   * Sigleton container
+   */
+  let instance; sdcsdcdscsd
 
+  /**
+   * @class Abstract represent contros for Scene class
+   * @throws abstractSelect custom event that define target rotation
+   */
+  class Abstract {
     /**
-     * Sigleton container
+     * @constructor define inner variables, method bindings and start init method on DOM load
      */
-    let instance;
+    constructor () {
+      if (typeof instance !== 'undefined') return instance;
+      instance = this;
 
-    /**
-     * @class Abstract represent contros for Scene class
-     * @throws abstractSelect custom event that define target rotation
-     */
-    class Abstract {
+      this.blocked = false;
+      this.animation_duration = 500;
 
-        /**
-         * @constructor define inner variables, method bindings and start init method on DOM load
-         */
-        constructor () {
-
-            if(typeof instance !== "undefined") return instance;
-            instance = this;
-
-            this.blocked = false;
-            this.animation_duration = 500;
-
-            for (const name of Object.getOwnPropertyNames(Object.getPrototypeOf(this))) {
-                const method = this[name];
-                if (name !== 'constructor' && typeof method === 'function') {
-                    this[name] = method.bind(this);
-                }
-            }
-
-            let ready = new Promise((resolve, reject) => {
-            if (document.readyState != "loading") return resolve();
-                document.addEventListener("DOMContentLoaded", () => resolve());
-            });
-            ready.then(this.init.bind(this));
+      for (const name of Object.getOwnPropertyNames(Object.getPrototypeOf(this))) {
+        const method = this[name];
+        if (name !== 'constructor' && typeof method === 'function') {
+          this[name] = method.bind(this);
         }
+      }
 
-        /**
-         * Preselect elements and add event listeners
-         */
-        init () {
-            this.component = document.querySelector('.abstract');
-            
-            if (this.component == null) return;
-
-            this.chapter = this.component.querySelector('.abstract__chapter--current');
-            for(let chapter of this.component.querySelectorAll('.abstract__chapter')) {
-                chapter.addEventListener('click', this.open);
-            }
-        }
-
-        /**
-         * Mark selected option as current and dispatch event for Scene class
-         * @param {Event} event mouse click or touch event
-         */
-        open (event) {
-            if (this.isBlocked()) return;
-
-            const chapter = event.currentTarget
-                , target = chapter.getAttribute('data-target');
-            
-            if (chapter.classList.contains('abstract__chapter--current')) return;
-
-            this.chapter.classList.remove('abstract__chapter--current');
-            this.chapter = chapter;
-            chapter.classList.add('abstract__chapter--current');
-
-            document.dispatchEvent(new CustomEvent('abstractSelect', { 'detail': target }));
-            
-            this.block();
-            setTimeout(this.unblock, this.animation_duration);
-        }
-
-        /**
-         * Block the controls, while animation playing
-         */
-        block () {
-            this.blocked = true;
-        }
-
-        /**
-         * Unblock the controls
-         */
-        unblock () {
-            this.blocked = false;
-        }
-
-        /**
-         * Check if controls is blocked
-         */
-        isBlocked () {
-            return this.blocked;
-        }
+      let ready = new Promise((resolve) => {
+        if (document.readyState !== 'loading') return resolve();
+        document.addEventListener('DOMContentLoaded', () => resolve());
+      });
+      ready.then(this.init.bind(this));
     }
-    new Abstract;
 
+    /**
+     * Preselect elements and add event listeners
+     */
+    init () {
+      this.component = document.querySelector('.abstract');
+
+      if (this.component === null) return;
+
+      this.chapter = this.component.querySelector('.abstract__chapter--current');
+      for (let chapter of this.component.querySelectorAll('.abstract__chapter')) {
+        chapter.addEventListener('click', this.open);
+      }
+    }
+
+    /**
+     * Mark selected option as current and dispatch event for Scene class
+     * @param {Event} event mouse click or touch event
+     */
+    open (event) {
+      if (this.isBlocked()) return;
+
+      const chapter = event.currentTarget
+        , target = chapter.getAttribute('data-target');
+
+      if (chapter.classList.contains('abstract__chapter--current')) return;
+
+      this.chapter.classList.remove('abstract__chapter--current');
+      this.chapter = chapter;
+      chapter.classList.add('abstract__chapter--current');
+
+      document.dispatchEvent(new CustomEvent('abstractSelect', { 'detail': target }));
+
+      this.block();
+      setTimeout(this.unblock, this.animation_duration);
+    }
+
+    /**
+     * Block the controls, while animation playing
+     */
+    block () {
+      this.blocked = true;
+    }
+
+    /**
+     * Unblock the controls
+     */
+    unblock () {
+      this.blocked = false;
+    }
+
+    /**
+     * Check if controls is blocked
+     */
+    isBlocked () {
+      return this.blocked;
+    }
+  }
+  new Abstract();
 })();
