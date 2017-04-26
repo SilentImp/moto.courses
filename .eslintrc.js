f4c2c402db6730efa7e2e92b7b0b411795dc565d
Anton Nemtsev <silentimp@MacBook-Pro-Anton.local>
2017-04-26 18:02:50 +0300

    config moved to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
+  "extends": "standard",
+  "plugins": [
+    "standard",
+    "promise"
+  ],
+  "env": {
+        "browser": true,
+        "es6": true
+  },
+  "rules": {
+    "one-var": 0,
+    "no-new": 0,
+    "semi": ["error", "always"],
+    "comma-style": ["error", "first"],
+    "padded-blocks": ["error", "never"]
+  }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -104,16 +104,7 @@ gulp.task('javascript', ['lint_js'], function() {
 
 gulp.task('lint_js', function() {
   return gulp.src( paths.source.js )
-    .pipe(eslint({
-      envs: ["browser", "es6"],
-      rules: {
-          "one-var": 0,
-          "no-new": 0,
-          "semi": ["error", "always"],
-          "comma-style": ["error", "first"],
-          "padded-blocks": ["error", "never"]
-      }
-    }))
+    .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
 });
