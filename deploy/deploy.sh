3e663083915e75c49b1c995bd9edf9309f6cb0e3
Anton Nemtsev <anton@moto.courses>
2017-04-29 16:57:02 +0300

    deploy changed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -16,22 +16,22 @@ cd /;
 cd ${WEB_PATH};
 tar -xzf ./${ARCH_NAME} -C ./;
 rm ./${ARCH_NAME};
+ls -dt */ | tail -n +5 | xargs rm -rf;
 if [ ! -f ".env" ]; then
     echo WEB_PATH=${WEB_PATH} >> .env;
 fi
 cd ${RESULT_DIR};
-nvm install 7
-nvm use 7
-nvm alias default 7
+nvm install 7;
+nvm use 7;
+nvm alias default 7;
 npm install;
 npm run build;
-cd ..
-rm -dRf ${SYMLINK_NAME}
-ln -ds ${RESULT_DIR} ./${SYMLINK_NAME}
-ls -dt */ | tail -n +11 | xargs rm -rf
-cd ./${SYMLINK_NAME}
-pm2 stop ${PROCESS_NAME}
-pm2 delete ${PROCESS_NAME}
-pm2 start ./server/server.js --name="${PROCESS_NAME}" --watch
-logout
+cd ..;
+rm -dRf ${SYMLINK_NAME};
+ln -ds ${RESULT_DIR} ./${SYMLINK_NAME};
+cd ./${SYMLINK_NAME};
+pm2 stop ${PROCESS_NAME};
+pm2 delete ${PROCESS_NAME};
+pm2 start ./server/server.js --name="${PROCESS_NAME}" --watch;
+logout;
 EOF
