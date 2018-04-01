03d71cf20f0d188525697af5a308a545740d38bd
Anton Nemtsev <thesilentimp@gmail.com>
2018-04-02 02:49:26 +0300

    lock

--- source/components/checkout/javascript/checkout.js
+++ source/components/checkout/javascript/checkout.js
@@ -9,6 +9,10 @@ if (Stripe !== undefined) {
     , requestPayerEmail: true
     , requestPayerPhone: true
     , requestShipping: false
+    , displayItems: {
+      amount: 4000
+      , label: 'maintenance.course'
+    }
     , total: {
       label: 'Мастеркласс'
       , amount: 4000
